c43fdd37fb3e6a6fee1985ee08cdacfabf314d1c
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-18 19:25:20 +0600

    Navbar design Complete

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import logo from "../../../assets/car-toy-market.png";
+import logo from "../../assets/car-toy-market.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-red-100">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -28,55 +28,42 @@ const Navbar = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link to={"/"}>Home</Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link to={"alltoys"}>All Toys</Link>
+              </li>
+              <li>
+                <Link to={"mytoys"}>My Toys</Link>
+              </li>
+              <li>
+                <Link to={"addtoys"}>Add A Toys</Link>
+              </li>
+              <li>
+                <Link to={"blogs"}>Blogs</Link>
               </li>
             </ul>
           </div>
           <Link to={"/"}>
-            <img src={logo} alt="" />
-          </Link>
+              <img src={logo} alt="" />
+            </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Home</a>
-            </li>
-            <li tabIndex={0}>
-              <a>All Toys</a>
+              <Link to={"/"}>Home</Link>
             </li>
             <li>
-              <a>My Toys</a>
+              <Link to={"alltoys"}>All Toys</Link>
             </li>
             <li>
-              <a>Add A Toys</a>
+              <Link to={"mytoys"}>My Toys</Link>
             </li>
             <li>
-              <a>Blogs</a>
+              <Link to={"addtoys"}>Add A Toys</Link>
+            </li>
+            <li>
+              <Link to={"blogs"}>Blogs</Link>
             </li>
           </ul>
         </div>
