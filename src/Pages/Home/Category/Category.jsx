d1031b3a7faa9374721c5ee365a84b8bb0e25078
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-19 11:38:06 +0600

    Category Design Complete

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import CategoryCard from "./CategoryCard";
 
 const Category = () => {
-  const [category, setCategory] = useState([]);
+  const [catagories, setCategory] = useState([]);
 
   useEffect(() => {
     fetch("car-toy.json")
@@ -12,6 +13,11 @@ const Category = () => {
 
   return (
     <>
+      <div className="text-center py-10">
+        {catagories.map((category) => (
+          <CategoryCard key={category.id} category={category}></CategoryCard>
+        ))}
+      </div>
       <div className=" w-full mx-auto bg-base-200">
         <Tabs>
           <TabList>
@@ -21,7 +27,18 @@ const Category = () => {
           </TabList>
 
           <TabPanel>
-            <h2>Any content 1</h2>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+              <figure>
+                <img
+                  src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                />
+              </figure>
+            </div>
           </TabPanel>
           <TabPanel>
             <h2>Any content 2</h2>
@@ -30,9 +47,6 @@ const Category = () => {
             <h2>Any content 3</h2>
           </TabPanel>
         </Tabs>
-      </div>
-      <div className="text-center py-10">
-        <p className="font-bold text-3xl">Category: {category.length}</p>
       </div>
     </>
   );
--- src/Pages/Home/Category/CategoryCard.jsx
+++ src/Pages/Home/Category/CategoryCard.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-const CategoryCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CategoryCard = ({ category }) => {
+  console.log(category.toy_name);
+
+  return (
+    <div className=" w-full mx-auto bg-base-200">
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+          <Tab>Title 3</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default CategoryCard;
