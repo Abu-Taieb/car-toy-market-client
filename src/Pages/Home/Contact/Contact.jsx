8b8ccd00904060c5a95be4a79c5050165f71546f
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-20 21:05:20 +0600

    Contact form  section design done

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-
 const Contact = () => {
   return (
     <div className="">
-      <h2 className="text-center font-bold text-3xl my-10">Contact With Us</h2>
-      <div className="bg-white flex justify-evenly p-10 rounded-xl">
-        <div className="w-1/3">
-          <h2 className="text-5xl text-black font-bold">Hello</h2>
+      <h2 className="text-center font-bold text-3xl my-10">Contact Us</h2>
+      <div className="bg-white flex flex-col lg:flex-row justify-evenly p-10 rounded-xl">
+        <div className="w-full lg:w-1/3 mr-10 flex items-center">
+          <h2 className="text-5xl text-black font-bold">If you interested! Please feel free to contact with us.</h2>
         </div>
-        <div className="flex bg-[#FF0126] p-10 gap-10 rounded-xl w-2/3">
-          <div className="text-white w-1/2">
-            <h3 className="font-bold text-3xl">
+        <div className="flex flex-col md:flex-row bg-base-content p-10 gap-10 rounded-xl w-full lg:w-2/3">
+          <div className="text-white w-full lg:w-1/2">
+            <h3 className="text-center md:text-left font-bold text-3xl">
               Lets discuss on something cool together
             </h3>
             <div className="grid grid-cols-1 gap-3 mt-20">
-                <h5 className="">email.contact@mail.com</h5>
-                <h5 className="">+88012 959 500</h5>
-                <h5 className="">Street #122, Road #122, Dhaka, Bangladesh</h5>
+              <h5 className="">email.contact@mail.com</h5>
+              <h5 className="">+88012 959 500</h5>
+              <h5 className="">Street #122, Road #122, Dhaka, Bangladesh</h5>
             </div>
           </div>
-          <div className="grid grid-cols-1 gap-5 bg-white p-10 rounded-xl w-1/2 ">
+          <div className="grid grid-cols-1 gap-5 bg-white p-10 rounded-xl w-full lg:w-1/2 ">
             <h5 className="font-bold">I am interested in...</h5>
             <input
               type="text"
@@ -40,7 +38,9 @@ const Contact = () => {
               placeholder="Your message"
               className="textarea textarea-bordered textarea-sm w-full "
             ></textarea>
-            <button className="btn btn-block bg-[#FF0126] border-0">Send Message</button>
+            <button className="btn btn-block bg-[#FF0126] border-0">
+              Send Message
+            </button>
           </div>
         </div>
       </div>
